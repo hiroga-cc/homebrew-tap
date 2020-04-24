90a37d276ef341332e3bf8502608c5885b655590
Hiroaki Ogasawara <hiroga1030@gmail.com>
2020-04-24 23:25:40 +0900

    checksum

--- Casks/curecon-qt.rb
+++ Casks/curecon-qt.rb
@@ -1,5 +1,6 @@
 cask 'curecon-qt' do
     version '2.0.0.2'
+    sha256 'daffb55354cc16cc715640b72d8fae593b319571e1d31b65c7487a89dd118251'
   
     url "https://github.com/cygnusxi/CurecoinSource/releases/download/v#{version}/curecoin-Qt-MacOSX.#{version}.zip"
     appcast 'https://github.com/stoplightio/desktop/releases.atom'
