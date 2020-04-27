2d9c961066bab775ad1f23ba1470278b6d38d183
Hiroaki Ogasawara <hiroga1030@gmail.com>
2020-04-27 20:14:12 +0900

    fix sha

--- Casks/cluster.rb
+++ Casks/cluster.rb
@@ -1,6 +1,6 @@
 cask 'cluster' do
     version '1.62'
-    sha256 'daffb55354cc16cc715640b72d8fae593b319571e1d31b65c7487a89dd118251'
+    sha256 '3fea2f68c4abc764a27d8795da39a71896667281b71f54355f21d6127d4fb470'
   
     url "https://update.cluster.mu/cluster.pkg"
     name 'Cluster, virtual SNS for multi platform'
