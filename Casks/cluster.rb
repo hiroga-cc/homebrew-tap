fbe2fea929c36906ecc788b877883ee71e7ee9e5
Hiroaki Ogasawara <hiroga1030@gmail.com>
2020-04-27 20:24:00 +0900

    use package instead of app

--- Casks/cluster.rb
+++ Casks/cluster.rb
@@ -3,8 +3,8 @@ cask 'cluster' do
     sha256 '3fea2f68c4abc764a27d8795da39a71896667281b71f54355f21d6127d4fb470'
   
     url "https://update.cluster.mu/cluster.pkg"
-    name 'Cluster, virtual SNS for multi platform'
+    name 'Cluster'
     homepage 'https://cluster.mu/'
   
-    app 'cluster.app'
+    pkg 'cluster.pkg'
   end
