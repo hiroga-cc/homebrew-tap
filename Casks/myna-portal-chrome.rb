88fc0520367aaf03e5d798242bece96aad71b49b
Hiroaki Ogasawara <hiroga1030@gmail.com>
2020-05-30 19:36:11 +0900

    マイナポータル

---
+++ Casks/myna-portal-chrome.rb
@@ -0,0 +1,19 @@
+cask 'myna-portal-chrome' do
+  version '8.2.0'
+  sha256 '1abcc1d25e8f1af03b30cdc44df43873ea30915a534049132ef0b58d5cb756e5'
+
+  url "https://img.myna.go.jp/tools/mac/MynaPortalAppSetup.dmg"
+  name 'マイナポータルAP'
+  homepage 'https://app.oss.myna.go.jp/Application/resources/dousakankyou/index.html'
+
+  app 'マイナポータルAP.app'
+
+  uninstall delete: [
+                      '/Library/Google/Chrome/NativeMessagingHosts/jp.go.cao.mpa.json',
+                    ]
+
+  caveats <<~EOS
+    Chromeで以下のURLにアクセスし、Chromeウェブストアから拡張機能をインストールしてください。
+    https://chrome.google.com/webstore/detail/fjjibalalngebiaophebgkkohaaggphf
+  EOS
+end
